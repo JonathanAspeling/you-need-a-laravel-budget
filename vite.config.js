0dad40becdf02596410cb2ddb308ea2eb8703626
Jonathan Aspeling <jbaspeling@gmail.com>
2023-06-05 22:07:17 +0200

    Initial Vite config setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    server: {
+        hmr: {
+            https:true,
+            host: 'localhost',
+            protocol: "ws"
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
